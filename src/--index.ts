0666de1c7d587624ed1de1fc6ab15ca2ffdfb975
Tais Reis <taisrsduarte@gmail.com>
2023-04-28 16:27:54 -0300

    feature: start bff more files

--- src/--index.ts
+++ src/--index.ts
@@ -1,13 +1,56 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { schema } from './graphql/schema';
+import { buildSubgraphSchema } from '@apollo/federation';
+import { gql } from 'apollo-server-express';
+import axios from 'axios';
+import humps from 'humps';
 
-const server = new ApolloServer({
-  schema
-});
+const typeDefs = `#graphql
+  type Movie {
+    id: Int
+    title: String
+    productionCompanies: [ProductionCompany]
+  }
+
+  type ProductionCompany {
+    id: Int
+    name: String
+    originCountry: String
+  }
+
+  type Query {
+    movie(id: Int!): Movie
+    movies: [Movie]
+  }
+`;
+
+const URL = 'https://api.themoviedb.org/3';
+const TOKEN = '16d5d710a1d2df0d971d0f77f928af7e';
+
+const resolvers = {
+  Query: {
+    movies: async () => { 
+      const moviesResponse = await axios.get(`${URL}/movie/popular?page=1&api_key=${TOKEN}`);
+      const movies = moviesResponse.data.results;
+
+      for (const movie of movies) {
+        const movieResponse = await axios.get(`${URL}/movie/${movie.id}?api_key=${TOKEN}`);
+        movie.production_companies = movieResponse.data.production_companies;
+      }
+
+      return humps.camelizeKeys(movies);
+    },
+    movie: async (_: any, { id }: { id: number }) => { 
+      const movieResponse = await axios.get(`${URL}/movie/${id}?api_key=${TOKEN}`);
+      return humps.camelizeKeys(movieResponse.data);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 3000 },
 });
 
-console.log(`🚀  Server ready at: ${url}`);
+console.log(`🚀 Server ready at: ${url}`);
