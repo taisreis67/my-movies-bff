31be799e4600f746142510fa0387fe2054c5f032
Tais Reis <taisrsduarte@gmail.com>
2023-04-28 15:51:01 -0300

    chore: save work

---
+++ src/--index.ts
@@ -0,0 +1,13 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { schema } from './graphql/schema';
+
+const server = new ApolloServer({
+  schema
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 3000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,69 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { schema } from './graphql/index';
+import { buildSubgraphSchema } from '@apollo/federation';
+import { gql } from 'apollo-server-express';
+import axios from 'axios';
 
-const server = new ApolloServer({
-  schema
-});
+const typeDefs = `#graphql
+  type Movie {
+    id: Int
+    title: String
+    productionCompany: [ProductionCompany]
+  }
+
+  type ProductionCompany {
+    id: Int
+    name: String
+    homepage: String
+  }
+
+  type Query {
+    movie(id: Int!): Movie
+    movies: [Movie]
+  }
+`;
+
+const URL = 'https://api.themoviedb.org/3';
+const TOKEN = '16d5d710a1d2df0d971d0f77f928af7e';
+
+const resolvers = {
+  Query: {
+    movies: async () => { 
+      const moviesResponse = await axios.get(`${URL}/movie/popular?page=1&api_key=${TOKEN}`);
+      const movies = moviesResponse.data.results;
+
+      for (const movie of movies) {
+        const movieResponse = await axios.get(`${URL}/movie/${movie.id}?api_key=${TOKEN}`);
+        const productsCompanies = movieResponse.data.production_companies;
+
+        for (const productsCompany of productsCompanies) {
+          const productCompanyResponse = await axios.get(`${URL}/company/${productsCompany.id}?api_key=${TOKEN}`);
+          movie.productionCompany.push(productCompanyResponse.data);
+        }
+      }
+
+      return movies;
+    },
+    movie: async (_: any, { id }: { id: number }) => { 
+      const movieResponse = await axios.get(`${URL}/movie/${id}?api_key=${TOKEN}`);
+      const movie = movieResponse.data;
+      const productsCompanies = movie.production_companies;
+      console.log(movie);
+
+      for (const productsCompany of productsCompanies) {
+        const productCompanyResponse = await axios.get(`${URL}/company/${productsCompany.id}?api_key=${TOKEN}`);
+        movie.productionCompany.push(productCompanyResponse.data);
+      }
+
+      return movie;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 3000 },
 });
 
-console.log(`🚀  Server ready at: ${url}`);
+console.log(`🚀 Server ready at: ${url}`);
